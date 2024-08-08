868efc81b241ca7b5522f13539921b06fade5df2
maitindrajit <indrajitmighty@gmail.com>
2024-08-08 23:49:00 +0530

    initial commit.

---
+++ Java_Basic_Questions/src/Sorting_Array.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Sorting_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr1 = {7,6,5,4,3,2,1};
+		Arrays.sort(arr1);
+		for(int i =0;i<arr1.length;i++) {
+			System.out.print(arr1[i]);
+		}
+
+	}
+
+}
