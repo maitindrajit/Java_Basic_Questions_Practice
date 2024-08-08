9930b1db145ca19e88fe5037364ed612427d21b8
maitindrajit <indrajitmighty@gmail.com>
2024-08-08 23:43:43 +0530

    Initial Commit.

---
+++ Java_Basic_Questions/src/String_to_Array.java
@@ -0,0 +1,14 @@
+
+public class String_to_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "My name is Khan";
+		char[] myarray = a.toCharArray();
+		for(int i=0;i<myarray.length;i++) {
+			System.out.println(myarray[i]);
+		}
+
+	}
+
+}
---
+++ Java_Basic_Questions/src/Sum_of_Array.java
@@ -0,0 +1,15 @@
+
+public class Sum_of_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {1,2,3,4,15};
+		int sum=0;
+		for(int i=0;i<arr.length;i++){
+			sum=sum+arr[i];
+		}
+		System.out.println("Sum of Array is: "+sum);
+
+	}
+
+}
